e17389dbaecee0f9c45ad5e1d0f7906d02fcdabf
tcdoggett <tcdoggett@gmail.com>
2026-01-23 20:33:43 -0500

    Update oligarchic_collision.py

--- src/planetary_system_data/oligarchic_collision.py
+++ src/planetary_system_data/oligarchic_collision.py
@@ -1,18 +1,21 @@
-# this sub-program covers step 12 ("Oligarchic Collision") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction" from pg. 67
+# this sub-program covers step 12 ("Oligarchic Collision") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World 
+# Design for Interstellar Fiction" from pg. 67
 
-# "The small, rocky planets of the inner system are the last to form, taking tens of millions of years to reach their final state. Inside the formation ice line, the dry 
-# planetesimals clump, forming a swarm of millions of planetoids. Later in the process, these planetoids coalesce and form a few dozen oligarchs, Mars-sized objects that 
-# take up a wide variety of orbits throughout the inner disk. Finally, these oligarchs in turn combine to form full-sized rocky planets. This process is chaotic and often 
-# extremely violent, involving many collisions and drastic changes in orbital parameters. This step places any planets (and related objects) which form by oligarchic 
-# collision in the evolving planetary system."
+# "The small, rocky planets of the inner system are the last to form, taking tens of millions of years to reach their final 
+# state. Inside the formation ice line, the dry planetesimals clump, forming a swarm of millions of planetoids. Later in the 
+# process, these planetoids coalesce and form a few dozen oligarchs, Mars-sized objects that take up a wide variety of orbits 
+# throughout the inner disk. Finally, these oligarchs in turn combine to form full-sized rocky planets. This process is 
+# chaotic and often extremely violent, involving many collisions and drastic changes in orbital parameters. This step places 
+# any planets (and related objects) which form by oligarchic collision in the evolving planetary system."
 
-# "Procedure - To begin, check to see whether either formation orbit 0, or the next formation orbit outward, is already occupied by a core-accretion planet as determined in 
-# Step Eleven. If so, then no planets will form in this step; skip forward to Step Thirteen"
+# "Procedure - To begin, check to see whether either formation orbit 0, or the next formation orbit outward, is already 
+# occupied by a core-accretion planet as determined in Step Eleven. If so, then no planets will form in this step; skip 
+# forward to Step Thirteen"
 
 # WRITE CODE HERE
 
-# "Potential Number of Planets: This sub-step determines how many terrestrial planets are likely to form. Beginning with the innermost formation orbit other than formation 
-# orbit 0, count formation orbits outward until the next formation orbit: 
+# "Potential Number of Planets: This sub-step determines how many terrestrial planets are likely to form. Beginning with the 
+# innermost formation orbit other than formation orbit 0, count formation orbits outward until the next formation orbit: 
 # - Is formation orbit 6
 # - Is already occupied by a core-accretion planet, or 
 # - Falls within a forbidden zone. 
@@ -20,9 +23,10 @@
 
 # WRTIE CODE HERE
 
-# "Next, roll 3d6 on the Oligarchic Collision Table, and apply the result to the number of formation orbits determined above. The result is the number of terrestrial planets 
-# that may form during the process of oligarchic collision. The descriptive terms indicate how tightly packed the orbits of the newly formed planets will be, early in the 
-# evolution of the planetary system"
+# "Next, roll 3d6 on the Oligarchic Collision Table, and apply the result to the number of formation orbits determined above. 
+# The result is the number of terrestrial planets that may form during the process of oligarchic collision. The descriptive 
+# terms indicate how tightly packed the orbits of the newly formed planets will be, early in the evolution of the planetary 
+# system"
 
 # Oligarchic Collision Table
 # 5 or Less - Very Loose; subtract 2 (minimum 0)
@@ -37,8 +41,9 @@
 
 # WRITE CODE HERE
 
-# "Place Oligarchic-Collision Planets - If the result from the Terrestrial Planets table was Tight or Very Tight, start with formation orbit 0. Otherwise, start with the 
-# next formation orbit outward. Place a planet on the worksheet in that formation orbit, then in each formation orbit outward, until: 
+# "Place Oligarchic-Collision Planets - If the result from the Terrestrial Planets table was Tight or Very Tight, start with 
+# formation orbit 0. Otherwise, start with the next formation orbit outward. Place a planet on the worksheet in that formation 
+# orbit, then in each formation orbit outward, until: 
 # - All the required planets have been placed
 # - The next formation orbit is already occupied by a core-accretion planet, or
 # - The next formation orbit falls into a forbidden zone.
@@ -46,6 +51,7 @@
 
 # WRITE CODE HERE
 
-# "If any planets indicated by the Oligarchic Collision Table fail to be placed, those planets were ejected from the planetary system early in its evolution."
+# "If any planets indicated by the Oligarchic Collision Table fail to be placed, those planets were ejected from the planetary 
+# system early in its evolution."
 
 # WRITE CODE HERE
