ad714baf04f70c63dccacfd200e861867ba863de
tcdoggett <tcdoggett@gmail.com>
2026-01-31 21:20:43 -0500

    Create import_from_Gaia.py

---
+++ src/planetary_system_data/import_from_Gaia.py
@@ -0,0 +1,5 @@
+# scenario #1: spectral type is found in SIMBAD
+
+# scenario #2: no spectral type is found in SIMBAD
+# would need to 'guesstimate' the spectral type based on other parameters from Gaia
+# e.g. compared to Hertzsprung-Russell Diagram diagram (luminosity vs temperature)
