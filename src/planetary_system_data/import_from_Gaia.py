e9888d048a95c176cf4e3d44ed960969b6d9a5e4
tcdoggett <tcdoggett@gmail.com>
2026-02-01 21:20:12 -0500

    Update import_from_Gaia.py

--- src/planetary_system_data/import_from_Gaia.py
+++ src/planetary_system_data/import_from_Gaia.py
@@ -1,5 +1,14 @@
-# scenario #1: spectral type is found in SIMBAD
+# scenario #A1: spectral type is found in SIMBAD
 
-# scenario #2: no spectral type is found in SIMBAD
+# scenario #A2: no spectral type is found in SIMBAD
 # would need to 'guesstimate' the spectral type based on other parameters from Gaia
 # e.g. compared to Hertzsprung-Russell Diagram diagram (luminosity vs temperature)
+# requirement: track whether spectral type is sourced from SIMBAD or generated from AOW
+
+# check for stellar age via SIMBAD
+# generate stellar age via procedure in AOW if not available via SIMBAD
+# requirement: track whether stellar age is sourced from SIMBAD or generated from AOW
+
+# check for metallicity via SIMBAD
+# generate metallicity via procedure in AOW if not available via SIMBAD
+# requirement: track whether metallicity is sourced from SIMBAD or generated from AOW
